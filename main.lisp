73944948bbeb77922628dffdcd26c5fde0a8d830
Vitor Silvestri <vitorsilvestri1@gmail.com>
2022-02-11 16:41:00 -0300

    algoritmo completo

--- main.lisp
+++ main.lisp
@@ -23,24 +23,24 @@
         (:print-function
             (lambda (struct stream depth)
                 (declare (ignore depth))
-                (format stream "~A-~A" (cell-group struct) (if (cell-value struct) (cell-value struct) "_"))
+                (format stream "~A-~A" (cell-block struct) (if (cell-value struct) (cell-value struct) "_"))
             )
         )
     )
-    group   ; Bloco
+    block   ; Bloco
     value   ; Valor
 )
 
 ; Cria uma célula conforme o padrão GRUPO-VALOR
 (defun cell-from-string(str)
-    (setq group "")
+    (setq block "")
     (setq value "")
 
     (setq current "")
     (loop for char across str do
         (cond
             ((eq char #\-)
-                (setq group current)
+                (setq block current)
                 (setq current "")
             )
             (t
@@ -55,7 +55,7 @@
     )
 
 
-    (setq cell (make-cell :group group :value value))
+    (setq cell (make-cell :block block :value value))
 )
 
 ; Separa uma string em uma lista de string, com base em um caractere
@@ -90,24 +90,150 @@
     (not (member nil (loop for row in (board-cells board) collect (loop for cell in row never (eq (cell-value cell) nil)))))
 )
 
+(defun valid-position(n i j)
+    (and (>= i 0) (>= j 0) (< i n) (< j n))
+)
+
 (defun board-at(board position)
-    (nth (nth 1 position) (nth (nth 0 position) (board-cells board)))
+    (let
+        (
+            (i (nth 0 position))
+            (j (nth 1 position))
+        )
+        (if (valid-position (board-size board) i j) (nth j (nth i (board-cells board))) nil)
+    )
+)
+
+(defun board-set(board position value)
+    (let
+        (
+            (i (nth 0 position))
+            (j (nth 1 position))
+            (cell (board-at board position))
+        )
+        (setf (nth j (nth i (board-cells board))) (make-cell :block (cell-block cell) :value value))
+    )
+)
+
+(defun value-at(board position)
+    (cell-value (board-at board position))
 )
 
 (defun board-nothing-at(board position)
     (eq nil (cell-value (board-at board position)))
 )
 
+; Retorna todas as células do tabuleiro, em uma lista unidimensional
+(defun board-cells-1d(board)
+    (loop for row in (board-cells board) nconcing (loop for cell in row collect cell))
+)
+
+; Retorna os possíveis números para um bloco
+(defun block-options(board block)
+    (let*
+        (
+            (block-cells (board-block board block))
+            (block-values (loop for cell in block-cells if (cell-value cell) collect (cell-value cell)))
+        )
+        (loop for i from 1 to (length block-cells) if (not (member i block-values)) collect i)
+    )
+)
+
+(defun get-neighbor-positions(i j)
+    (loop for a from -1 to 1 nconcing (loop for b from -1 to 1 if (not (and (= a 0) (= b 0))) collect (list (+ i a) (+ j b))))
+)
+
+(defun get-neighbors(board i j)
+    (loop for neighbor in (loop for position in (get-neighbor-positions i j) collect (board-at board position)) if neighbor collect neighbor)
+)
+
+(defun get-neighbor-values(board i j)
+    (loop for neighbor in (get-neighbors board i j) if (cell-value neighbor) collect (cell-value neighbor))
+)
+
+; Retorna todas as células de um bloco
+(defun board-block(board block)
+    (loop for cell in (board-cells-1d board) if (string= (cell-block cell) block) collect cell)
+)
+
+(defun get-cell-options(board position)
+    (let 
+        (
+            (cell (board-at board position))
+            (i (nth 0 position))
+            (j (nth 1 position))
+        )
+        (loop for option in (block-options board (cell-block cell)) if (not (member option (get-neighbor-values board i j))) collect option)
+    )
+)
+
+; Checa todas as células do tabuleiro retornando as que não foram respondidas ainda
+; Retorna a posição com menos opções, ou nil caso não haja mais posições livres
 (defun get-next-cell(board)
-    (loop for i from 0 below (board-size board) nconcing (loop for j from 0 below (board-size board) if (board-nothing-at board (list i j)) collect (list i j)))
+    (reduce (lambda (a b) (get-next-cell* board a b)) (loop for i from 0 below (board-size board) nconcing (loop for j from 0 below (board-size board) if (board-nothing-at board (list i j)) collect (list i j))))
+)
+
+; Itera sobre as células não respondidas, retornando a posição com menos possibilidades
+(defun get-next-cell*(board position-a position-b)
+    (cond
+        ((null position-a)
+            nil
+        )
+        ((null position-b)
+            nil
+        )
+        (t
+            (let
+                (
+                    (a (get-cell-options board position-a))
+                    (b (get-cell-options board position-b))
+                )
+                (if (< (length a) (length b)) position-a position-b)
+            )
+        )
+    )
 )
 
 ; Função responsável por resolver um tabuleiro, caso haja solução
-;; (defun solve(board)
-;;     (princ "Resolvendo o tabuleiro...") 
-;; )
+(defun solve(board)
+    (if
+        (board-solved board)
+        board
+        (let*
+            (
+                (pos (get-next-cell board))
+                (options (get-cell-options board pos))
+            )
+            (solve* board pos options)
+        )
+    )
+)
+
+(defun solve*(board pos options)
+    (cond
+        ((null options)
+            nil
+        )
+        (t
+            (let
+                ((value (pop options)))
+                (board-set board pos value)
+                (case (solve board)
+                    ((nil)
+                        (board-set board pos nil)
+                        (solve* board pos options)
+                    )
+                    (otherwise
+                        t
+                    )
+                )
+            )
+        )
+    )
+)
 
 (setq my-board (board-load 6 "Examples/6x6.txt"))
+
 (print my-board)
-(print (board-solved my-board))
-(print (get-next-cell my-board))
+(solve my-board)
+(print my-board)
